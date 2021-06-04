cd5a1b886824efaded9c8e2a53df04c2046f9094
junhee_lee2 <junhee_lee2@tmax.co.kr>
2021-06-04 14:28:38 +0900

    #1.1 apply styled component

--- src/routes/Home.js
+++ src/routes/Home.js
@@ -1,5 +1,41 @@
 import React from "react";
 import { gql, useQuery } from "@apollo/client";
+import styled from "styled-components";
+
+const Container = styled.div`
+display: flex;
+flex-direction: column
+align-items: center;
+width: 100%
+`;
+
+const Header = styled.header`
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  height: 45vh;
+  color: white;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 100%;
+`;
+
+const Title = styled.h1`
+  font-size: 60px;
+  font-weight: 600;
+  margin-bottom: 20px;
+`;
+
+const Subtitle = styled.h3`
+  font-size: 35px;
+`;
+
+const Loading = styled.div`
+  font-size: 18px;
+  opacity: 0.5;
+  font-weight: 500;
+  margin-top: 10px;
+`;
 
 const GET_MOVIES = gql`
   {
@@ -12,11 +48,13 @@ const GET_MOVIES = gql`
 
 export default () => {
   const { loading, data } = useQuery(GET_MOVIES);
-  console.log(loading, data);
-  if (loading) {
-    return "loading...";
-  }
-  if (data && data.movies) {
-    return data.movies.map((m) => <h1>{m.id}</h1>);
-  }
+  return (
+    <Container>
+      <Header>
+        <Title>Apollo Movie App</Title>
+        <Subtitle>I love GraphQL</Subtitle>
+      </Header>
+      {loading && <Loading>Loading...</Loading>}
+    </Container>
+  );
 };
