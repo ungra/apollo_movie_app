cdc2b8b078c8892dd73f6eacfa8ecb41bd52fc3e
Jun Hee Lee <junheelee@Junui-iMac.local>
2021-06-07 22:58:10 +0900

    #1.3 Apollo Cache and Styles

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { useParams } from "react-router-dom";
 import { gql, useQuery } from "@apollo/client";
+import styled from "styled-components";
 
 const GET_MOVIE = gql`
   query getMovie($id: Int!) {
@@ -8,9 +9,45 @@ const GET_MOVIE = gql`
       id
       title
       medium_cover_image
+      language
+      rating
       description_intro
     }
   }
+`;
+
+const Container = styled.div`
+  height: 100vh;
+  background-image: linear-gradient(-45deg, #d754ab, #fd723a);
+  width: 100%;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  color: white;
+`;
+
+const Column = styled.div`
+  margin-left: 10px;
+`;
+
+const Title = styled.h1`
+  font-size: 65px;
+  margin-bottom: 15px;
+`;
+
+const Subtitle = styled.h4`
+  font-size: 35px;
+  margin-bottom: 10px;
+`;
+
+const Description = styled.p`
+  font-size: 28px;
+`;
+
+const Poster = styled.div`
+  width: 25%;
+  height: 60%;
+  background-color: transparent;
 `;
 
 export default () => {
@@ -19,10 +56,14 @@ export default () => {
   const { loading, data } = useQuery(GET_MOVIE, {
     variables: { id },
   });
-  if (loading) {
-    return "loading...";
-  }
-  if (data && data.movie) {
-    return data.movie.title;
-  }
+  return (
+    <Container>
+      <Column>
+        <Title>Name</Title>
+        <Subtitle>English 4.5</Subtitle>
+        <Description>asdjf l;adsj l;f</Description>
+      </Column>
+      <Poster></Poster>
+    </Container>
+  );
 };
