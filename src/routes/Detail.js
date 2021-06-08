4b6c7d1e607a6e56840739efa73326e5e1605588
junhee_lee2 <junhee_lee2@tmax.co.kr>
2021-06-08 17:18:11 +0900

    #1.5 Suggestions & Optional Chaining

--- src/routes/Detail.js
+++ src/routes/Detail.js
@@ -13,6 +13,10 @@ const GET_MOVIE = gql`
       rating
       description_intro
     }
+    suggestions(id: $id) {
+      id
+      medium_cover_image
+    }
   }
 `;
 
@@ -66,16 +70,12 @@ export default () => {
         <Title>
           {loading ? "Loading..." : data.movie.title}
         </Title>
-        {!loading && data.movie && (
-          <>
-            <Subtitle>
-              {data.movie.language} · {data.movie.rating}
-            </Subtitle>
-            <Description>
-              {data.movie.description_intro}
-            </Description>
-          </>
-        )}
+        <Subtitle>
+          {data?.movie?.language} · {data?.movie?.rating}
+        </Subtitle>
+        <Description>
+          {data?.movie?.description_intro}
+        </Description>
       </Column>
       <Poster
         bg={
